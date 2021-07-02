8598a0b1da0cd0c64d4fa0a275d1734f0a4077a5
Shilluê <shilluegithub@gmail.com>
2021-07-02 10:20:46 -0300

    adicionado o código
    Javascript, HTML e CSS.

---
+++ codigo/script.js
@@ -0,0 +1,19 @@
+function digitar(n){
+    var numero = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = numero + n;
+}
+function limpar(){
+    document.getElementById("resultado").innerHTML = "";
+}
+ function apagar(){
+     var result = document.getElementById("resultado").innerHTML;
+     result = document.getElementById("resultado").innerHTML = result.substring(0, result.length - 1);
+ }
+ function calcular(){
+     var resultado = document.getElementById("resultado").innerHTML;
+     if(resultado){
+         document.getElementById("resultado").innerHTML = eval(resultado)
+     } else {
+         document.getElementById("resultado").innerHTML = "";
+     }
+ }
